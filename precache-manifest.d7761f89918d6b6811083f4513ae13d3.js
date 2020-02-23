4c26d0be4baedf1d5b0ff686487f9a644556828e
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-02-23 15:18:46 +0200

    Updates

---
+++ precache-manifest.d7761f89918d6b6811083f4513ae13d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5eca0b9fd5eacb4537873a3c5617221",
+    "url": "/dandelion/index.html"
+  },
+  {
+    "revision": "2c390c9e4d189fc367c8",
+    "url": "/dandelion/static/css/main.5c356352.chunk.css"
+  },
+  {
+    "revision": "041844c0e2e4487aaace",
+    "url": "/dandelion/static/js/2.88eee18f.chunk.js"
+  },
+  {
+    "revision": "2c390c9e4d189fc367c8",
+    "url": "/dandelion/static/js/main.2bc48f4e.chunk.js"
+  },
+  {
+    "revision": "6afbc7c31f738172e2eb",
+    "url": "/dandelion/static/js/runtime-main.975f953c.js"
+  },
+  {
+    "revision": "a9411b19da04aa84e80f41b822a5f01d",
+    "url": "/dandelion/static/media/Dandelion-logo.a9411b19.png"
+  },
+  {
+    "revision": "305ddba0f9165a21692cbcb75ca39430",
+    "url": "/dandelion/static/media/Kulbaba-foto.305ddba0.png"
+  }
+]);
