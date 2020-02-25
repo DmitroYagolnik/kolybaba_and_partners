9a6c0582383694b7c27cfa418fa8da3e49ccd4cf
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-02-25 19:40:10 +0200

    Change component "ChangeLanguageBtn"

--- src/components/ChangeLanguageBtn/ChangeLanguageBtn.jsx
+++ src/components/ChangeLanguageBtn/ChangeLanguageBtn.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './ChangeLanguageBtn.module.css';
 
+// const ChangeLanguageBtn = ({ changeLanguage, siteLanguage }) => {
+//   const styleBtn =
+//     siteLanguage === 'uk' ? style.changeLanguageRu : style.changeLanguageUk;
+//   return <button type="button" onClick={changeLanguage} className={styleBtn} />;
+// };
+
 const ChangeLanguageBtn = ({ changeLanguage, siteLanguage }) => {
-  const styleBtn =
-    siteLanguage === 'uk' ? style.changeLanguageRu : style.changeLanguageUk;
-  return <button type="button" onClick={changeLanguage} className={styleBtn} />;
+  const styleIndicatorUA =
+    siteLanguage === 'uk' ? style.activeIndicator : style.passiveIndicator;
+  const styleIndicatorRU =
+    siteLanguage === 'ru' ? style.activeIndicator : style.passiveIndicator;
+  return (
+    <button
+      type="button"
+      onClick={changeLanguage}
+      className={style.changeLanguageBtn}
+    >
+      <span className={styleIndicatorUA}>UA</span>/
+      <span className={styleIndicatorRU}>RU</span>
+    </button>
+  );
 };
 
 ChangeLanguageBtn.propTypes = {
