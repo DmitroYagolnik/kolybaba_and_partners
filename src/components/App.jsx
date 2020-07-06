4605f41eb4a008a3fee46f561085636e04eca059
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-07-06 20:35:59 +0300

    code splitting in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component, lazy, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Header from './Header/Header';
 import PageNavLink from './PageNavLink/PageNavLink';
 import Footer from './Footer/Footer';
 import withServicesNavLink from './withServicesNavLink/withServicesNavLink';
-import AboutUs from '../pages/AboutUs/AboutUs';
-import Services from '../pages/Services/Services';
-import DeclarationMTB from '../pages/DeclarationMTB/DeclarationMTB';
-import PermissionUseHazardousEquipment from '../pages/PermissionUseHazardousEquipment/PermissionUseHazardousEquipment';
-import PermissionHazardousWorkAndEquipmen from '../pages/PermissionHazardousWorkAndEquipment/PermissionHazardousWorkAndEquipmen';
-import IdentificationHFAndPHF from '../pages/IdentificationHFAndPHF/IdentificationHFAndPHF';
-import DevelopmentPLER from '../pages/DevelopmentPLER/DevelopmentPLER';
-import MedicalLicense from '../pages/MedicalLicense/MedicalLicense';
-import BuildingLicense from '../pages/BuildingLicense/BuildingLicense';
-import LicensePrecursors from '../pages/LicensePrecursors/LicensePrecursors';
-import CertificateISO9001 from '../pages/CertificateISO9001/CertificateISO9001';
-import CertificateISO22000 from '../pages/CertificateISO22000/CertificateISO22000';
-import CertificateISO14001 from '../pages/CertificateISO14001/CertificateISO14001';
-import ErrorPage from '../pages/ErrorPage/ErrorPage';
 import '../index.css';
 import i18n from '../i18n';
 import language from '../utils/language';
+
+const AboutUs = lazy(() => import('../pages/AboutUs/AboutUs'));
+const Services = lazy(() => import('../pages/Services/Services'));
+const DeclarationMTB = lazy(() =>
+  import('../pages/DeclarationMTB/DeclarationMTB'),
+);
+const PermissionUseHazardousEquipment = lazy(() =>
+  import(
+    '../pages/PermissionUseHazardousEquipment/PermissionUseHazardousEquipment'
+  ),
+);
+const PermissionHazardousWorkAndEquipmen = lazy(() =>
+  import(
+    '../pages/PermissionHazardousWorkAndEquipment/PermissionHazardousWorkAndEquipmen'
+  ),
+);
+const IdentificationHFAndPHF = lazy(() =>
+  import('../pages/IdentificationHFAndPHF/IdentificationHFAndPHF'),
+);
+const DevelopmentPLER = lazy(() =>
+  import('../pages/DevelopmentPLER/DevelopmentPLER'),
+);
+const MedicalLicense = lazy(() =>
+  import('../pages/MedicalLicense/MedicalLicense'),
+);
+const BuildingLicense = lazy(() =>
+  import('../pages/BuildingLicense/BuildingLicense'),
+);
+const LicensePrecursors = lazy(() =>
+  import('../pages/LicensePrecursors/LicensePrecursors'),
+);
+const CertificateISO9001 = lazy(() =>
+  import('../pages/CertificateISO9001/CertificateISO9001'),
+);
+const CertificateISO22000 = lazy(() =>
+  import('../pages/CertificateISO22000/CertificateISO22000'),
+);
+const CertificateISO14001 = lazy(() =>
+  import('../pages/CertificateISO14001/CertificateISO14001'),
+);
 
 export default class App extends Component {
   state = {
@@ -54,57 +80,59 @@ export default class App extends Component {
             changeLanguage={this.toggleLanguage}
             siteLanguage={siteLanguage}
           />
-          <Switch>
-            <Route exact path="/dandelion/" component={Services} />
-            <Route path="/dandelion/about_us" component={AboutUs} />
-            <Route
-              path="/dandelion/services/declaration_MTB"
-              component={withServicesNavLink(DeclarationMTB)}
-            />
-            <Route
-              path="/dandelion/services/permission_use_hazardous_equipment"
-              component={withServicesNavLink(PermissionUseHazardousEquipment)}
-            />
-            <Route
-              path="/dandelion/services/permission_hazardous_work_and_equipment"
-              component={withServicesNavLink(
-                PermissionHazardousWorkAndEquipmen,
-              )}
-            />
-            <Route
-              path="/dandelion/services/identification_HF_and_PHF"
-              component={withServicesNavLink(IdentificationHFAndPHF)}
-            />
-            <Route
-              path="/dandelion/services/development_PLER"
-              component={withServicesNavLink(DevelopmentPLER)}
-            />
-            <Route
-              path="/dandelion/services/medical_license"
-              component={withServicesNavLink(MedicalLicense)}
-            />
-            <Route
-              path="/dandelion/services/building_license"
-              component={withServicesNavLink(BuildingLicense)}
-            />
-            <Route
-              path="/dandelion/services/license_precursors"
-              component={withServicesNavLink(LicensePrecursors)}
-            />
-            <Route
-              path="/dandelion/services/certificate_ISO_9001"
-              component={withServicesNavLink(CertificateISO9001)}
-            />
-            <Route
-              path="/dandelion/services/certificate_ISO_22000"
-              component={withServicesNavLink(CertificateISO22000)}
-            />
-            <Route
-              path="/dandelion/services/certificate_ISO_14001"
-              component={withServicesNavLink(CertificateISO14001)}
-            />
-            <Route component={withServicesNavLink(ErrorPage)} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path="/dandelion" component={Services} />
+              <Route path="/dandelion/about_us" component={AboutUs} />
+              <Route
+                path="/dandelion/services/declaration_MTB"
+                component={withServicesNavLink(DeclarationMTB)}
+              />
+              <Route
+                path="/dandelion/services/permission_use_hazardous_equipment"
+                component={withServicesNavLink(PermissionUseHazardousEquipment)}
+              />
+              <Route
+                path="/dandelion/services/permission_hazardous_work_and_equipment"
+                component={withServicesNavLink(
+                  PermissionHazardousWorkAndEquipmen,
+                )}
+              />
+              <Route
+                path="/dandelion/services/identification_HF_and_PHF"
+                component={withServicesNavLink(IdentificationHFAndPHF)}
+              />
+              <Route
+                path="/dandelion/services/development_PLER"
+                component={withServicesNavLink(DevelopmentPLER)}
+              />
+              <Route
+                path="/dandelion/services/medical_license"
+                component={withServicesNavLink(MedicalLicense)}
+              />
+              <Route
+                path="/dandelion/services/building_license"
+                component={withServicesNavLink(BuildingLicense)}
+              />
+              <Route
+                path="/dandelion/services/license_precursors"
+                component={withServicesNavLink(LicensePrecursors)}
+              />
+              <Route
+                path="/dandelion/services/certificate_ISO_9001"
+                component={withServicesNavLink(CertificateISO9001)}
+              />
+              <Route
+                path="/dandelion/services/certificate_ISO_22000"
+                component={withServicesNavLink(CertificateISO22000)}
+              />
+              <Route
+                path="/dandelion/services/certificate_ISO_14001"
+                component={withServicesNavLink(CertificateISO14001)}
+              />
+              <Redirect to="/dandelion" />
+            </Switch>
+          </Suspense>
         </main>
         <Footer />
       </>
--- src/pages/ErrorPage/ErrorPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ErrorPage = () => <p>Вибачте, але дану сторінку не знайдено</p>;
-
-export default ErrorPage;
