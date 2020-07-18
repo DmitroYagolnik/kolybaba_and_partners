ae149d767d4b25b1d03d4cc678b34e9cc1143157
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-07-19 00:51:47 +0300

    added favicon

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import Favicon from 'react-favicon';
 import App from './components/App';
+import favicon from './img/favicon.ico';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <>
+    <Favicon url={favicon} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>,
   document.getElementById('root'),
 );
