07253975f90baeb3b13754888b58697f955c673b
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-11-22 17:39:38 +0200

    Banner added on DeclarationMTB page

--- src/pages/auxiliaryPages/occupationalHealth/DeclarationMTB/DeclarationMTB.jsx
+++ src/pages/auxiliaryPages/occupationalHealth/DeclarationMTB/DeclarationMTB.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import BannerImg from '../../../../img/banner/DeclarationMTB.jpg';
 
 const DeclarationMTB = () => {
   const { t } = useTranslation();
@@ -8,6 +9,14 @@ const DeclarationMTB = () => {
       <h2 className="page_services-title">
         {t('Occupational_health.Declaration_MTB-link')}
       </h2>
+
+      <div className="banner_img-wrapper">
+        <img
+          className="banner-img"
+          src={BannerImg}
+          alt="Ведеться небезпечна робота"
+        />
+      </div>
       <p className="page-text">{t('DeclarationMTB.paragraph_1')}</p>
       <p className="page-text">
         {t('DeclarationMTB.paragraph_2.part_1')}
