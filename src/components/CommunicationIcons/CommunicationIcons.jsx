ee48a29f7e692c1271afc289f32283571107f19a
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-12-06 16:28:19 +0200

    del Viber

--- src/components/CommunicationIcons/CommunicationIcons.jsx
+++ src/components/CommunicationIcons/CommunicationIcons.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { isMobileOnly } from 'react-device-detect';
 import whitePhoneIcon from '../../img/communication_icons/whitePhone_icon.png';
-import viberIcon from '../../img/communication_icons/viber_icon.png';
 import emailIcon from '../../img/communication_icons/email_icon.png';
 import contactInformation from '../../servises/additionalInformation/contactInformation.json';
 import style from './CommunicationIcons.module.css';
 
 const CommunicationIcons = ({ handlePhoneBtn }) => {
   const emailAddress = `mailto:${contactInformation.email}`;
-  const viberAddress = `viber://chat?number=${contactInformation.viber_number}`;
   return (
     <div className={style['Communication-icons-wrapper']}>
       {isMobileOnly && (
@@ -23,9 +21,6 @@ const CommunicationIcons = ({ handlePhoneBtn }) => {
       )}
       <a href={emailAddress}>
         <img src={emailIcon} alt="Написати на електронну пошту" />
-      </a>
-      <a href={viberAddress}>
-        <img src={viberIcon} alt="Viber" />
       </a>
     </div>
   );
